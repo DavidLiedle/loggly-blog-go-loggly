7cd1e9bc158f80b52ed87ad1bab940703b25e848
David Liedle <david.liedle@gmail.com>
2015-08-30 09:40:09 -0600

    stubs
    Getting things in place

---
+++ package.go
@@ -0,0 +1,7 @@
+/*
+ * An example of logging to Loggly in GoLang
+ * Supports content in Loggly's Blog authored by
+ * David Christian Liedle ( http://davidcanhelp.me/ )
+ */
+
+package "https://github.com/macdaddy/loggly-blog-go-loggly"
