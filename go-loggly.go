fe61ed5b9dc588eba2af59af778e2414ea6f68a3
David Liedle <david.liedle@gmail.com>
2015-09-13 00:17:53 -0600

    http://golang.org/doc/articles/wiki/#tmp_3

--- go-loggly.go
+++ go-loggly.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	
+}
+
 func main() {
-	fmt.Printf("Let's log to Loggly!\n")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+	
 }
